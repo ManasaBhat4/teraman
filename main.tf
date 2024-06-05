5c6c9570d2d893a9f19b62d10927f258a98dd404
Manasa Bhat <113411403+ManasaBhat4@users.noreply.github.com>
2024-06-05 15:59:29 +0530

    apply.tf

--- main.tf
+++ main.tf
@@ -4,17 +4,9 @@ region = "us-east-1"
 resource "aws_vpc" "sub-vpc" {
   cidr_block = "10.0.0.0/16"
 tags = {
-    Name = "production"
+    Name = "production1"
   }
 }
-resource "aws_subnet" "subnet-1" {
-  vpc_id     =aws_vpc.sub-vpc.id
-  cidr_block = "10.0.1.0/24"
-  availability_zone = "us-east-1a"
 
-  tags = {
-    Name = "simply"
-  }
-}
 
 
